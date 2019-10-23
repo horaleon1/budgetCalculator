24a98843e77e73c852f1256792f759fe22c8ba44
horaleon1 <horaleon1@gmail.com>
2019-10-22 19:45:21 -0500

    FEA/uuid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React from 'react';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import Alert from './components/Alert';
+import uuid from 'uuid/v4'
+
+const initialExpenses = [
+  {id:uuid(), charge:"rent", amount:1600},
+  {id:uuid(), charge:"car payment", amount:400},
+  {id:uuid(), charge:"credit card bill", amount:1200}  
+]
+
+console.log(initialExpenses);
 
 const App = () => {
-  return <div>
-    
-  </div>;
-};
 
-export default App;
+
+  return (
+    <div>
+      <Alert />
+     <ExpenseForm /> 
+     <ExpenseList />    
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem';
+
+const ExpenseList = () => {
+  return (
+    <div>list
+      <ExpenseItem />
+    </div>
+  )
+}
+
+export default ExpenseList
