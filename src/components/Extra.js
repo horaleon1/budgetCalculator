8dadc7ed938701e3c9a19cfe4a7e6eaeb449ce59
horaleon1 <horaleon1@gmail.com>
2019-10-26 14:39:46 -0500

    fes trys

--- src/components/Extra.js
+++ src/components/Extra.js
@@ -3,33 +3,53 @@ import axios from "axios";
 import Title from "./Title";
 
 const urlCategories = "/services/api/v1/market/categories/";
+const urlProducts = "/services/api/v1/market/products/"
 
 export default class extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categorias: ""
+      categorias: "",
+      categories: false,
+      productos: ""
     };
   }
 
   componentDidMount() {
-    axios(urlCategories).then(res => {
+    axios(urlCategories)
+    .then(res => {
       const categorias = res.data;
       const categorias2 = res;
       this.setState({ categorias });
-      console.log(categorias2);
+      console.log(categorias);
     });
+
+    axios(urlProducts)
+    .then(res => {
+      const productos = res.data
+      this.setState({ productos });
+      console.log(productos);
+    })
   }
+
+  activateCategories = e => {
+    this.setState({ categories: !this.state.categories });
+  };
 
   render() {
     return (
       <div>
         <Title title={"hola"} />
-        <ul>
-          {Object.keys(this.state.categorias).map((object, i) => (
-            <li key={i}>{this.state.categorias[object].name}</li>
-          ))}
-        </ul>
+        <button onClick={() => this.activateCategories()}>Categorias</button>
+
+        {this.state.categories ? (
+          <ul>
+            {Object.keys(this.state.categorias).map((object, i) => (
+              <li key={i}>{this.state.categorias[object].name}</li>
+            ))}
+          </ul>
+        ) : null}
+
         <Title title={"adios"} />
         <Title title={"thrid-B"} />
       </div>
